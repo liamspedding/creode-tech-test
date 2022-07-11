a040ea876f1017abc60cb56bedf94eebfaa30980
Liam Spedding <l.spedding@tig.uk.net>
2022-07-11 14:09:26 +0100

    Create foreach loop for Friends and Followers. Add Tailwind CSS and style frontend.

--- index.php
+++ index.php
@@ -5,4 +5,33 @@
     include_once('views/layout/header.php');
 ?>
 
+<?php
+
+    // Instantiate CBook Class
+    $cbook = new CBookAPI('8XWr1Hp5xbhSM6u0', '07xdHM9OA8ULaBeC');
+
+    // Loop through Friends data from API
+    echo '<h2 class="text-3xl font-bold mb-5">Friends</h2>';
+    echo '<div class="grid lg:grid-cols-3 gap-5 md:grid-cols-1">';
+    foreach ($cbook->getFriends() as $friends) {
+        foreach ($friends as $friend) {
+            include('views/friend.php');
+        }
+    }
+    echo '</div>';
+
+    // Instantiate Critter Class
+    $critter = new CritterAPI('j4nHNuaPo2nq6AdW');
+
+    echo '<h2 class="text-3xl font-bold mb-5">Followers</h2>';
+    echo '<div class="grid lg:grid-cols-3 gap-5 md:grid-cols-1">';
+    foreach ($critter->getFollowers() as $followers) {
+        foreach ($followers as $follower){
+            include('views/follower.php');
+        }
+    }
+    echo '</div>';
+
+?>
+
 <?php include_once('views/layout/footer.php'); ?>
