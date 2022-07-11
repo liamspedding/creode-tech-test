953bf642d93649f06721630b1d90e7efe26871c7
Liam Spedding <l.spedding@tig.uk.net>
2022-07-11 14:06:20 +0100

    Create index.php, add HTML boilerplate, and include composer autoloading file.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    // Autoload Classes using Composer
+    include_once 'vendor/autoload.php';
+
+    include_once('views/layout/header.php');
+?>
+
+<?php include_once('views/layout/footer.php'); ?>
